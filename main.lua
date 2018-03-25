10bbd0b4a964767678c4719ffd7f428e967b9a47
Sasha Malko <sasha.malko@ocsbstudent.ca>
2018-03-25 16:46:16 -0400

    added game over

--- main.lua
+++ main.lua
@@ -28,6 +28,18 @@ local incorrectAnswer
 local randomOperator
 local points = 0
 local pointsObject
+local totalSeconds = 10
+local secondsLeft = 10
+local clockText
+local countDownTimer
+local lives = 5
+local heart1
+local heart2
+local heart3
+local heart4
+local gameOver
+local gameOverSound = audio.loadSound("Sounds/GameOver.mp3")
+local gameOverSoundChannel
 
 ----------------------------------------------------------------------------------------------
 --LOCAL FUNCTIONS
@@ -64,7 +76,6 @@ local function AskQuestion()
     end
 end
 
-
 local function HideCorrect()
 	correctObject.isVisible = false
 	AskQuestion()
@@ -95,14 +106,64 @@ local function NumericFieldListener(event)
 			timer.performWithDelay(1000,HideCorrect)
 			points = points + 1
 			pointsObject.text = "Points" .. " = ".. points
-			pointsObject:setTextColor(27/255,144/255,35/255)
+			pointsObject:setTextColor(27/255, 144/255, 35/255)
 
 		elseif (userAnswer) then
 			correctObject.isVisible = false
 			incorrectObject.isVisible = true
 			timer.performWithDelay(1000,HideIncorrect)
+			lives = lives - 1
+        if (lives == 4) then
+	    	heart4.isVisible = false
+        elseif (lives == 3) then
+	    	heart3.isVisible = false
+	    	elseif (lives == 2) then 
+	    		heart2.isVisible = false
+	    		elseif (lives == 1) then
+	    			heart1.isVisible = false
+	    			gameOver.isVisible = true	
+	    			numericField.isVisible = false
+	    			clockText.isVisible = false
+	    			gameOverSoundChannel = audio.play(gameOverSound)
+	        end 
 		end
 	end
+end
+
+local function UpdateTime()
+
+	--decrement the number of seconds
+	secondsLeft = secondsLeft - 1
+
+	--display the number of seconds left in the clock object
+	clockText.text = secondsLeft .. "" 
+
+	if (secondsLeft == 0) then
+		--reset the number of seconds left
+		secondsLeft = totalSeconds
+	    lives = lives - 1
+	    AskQuestion()
+
+	    if (lives == 4) then
+	    	heart4.isVisible = false
+        elseif (lives == 3) then
+	    	heart3.isVisible = false
+	    	elseif (lives == 2) then 
+	    		heart2.isVisible = false
+	    		elseif (lives == 1) then
+	    			heart1.isVisible = false
+	    			gameOver.isVisible = true
+	    			numericField.isVisible = false
+	    			clockText.isVisible = false
+	    			gameOverSoundChannel = audio.play(gameOverSound)			
+	    end
+    end
+end
+
+--function that calls the timer
+local function StartTimer()
+	--create a countdown timer that loops infinitely
+	countDownTimer = timer.performWithDelay(1000, UpdateTime, 0)
 end
 
 ----------------------------------------------------------------------------------------------
@@ -114,8 +175,8 @@ questionObject = display.newText("", display.contentWidth/3, display.contentHeig
 questionObject:setTextColor(27/255, 71/255, 144/255)
 
 --displays the points and sets the colour
-pointsObject = display.newText("", 500, 250, Arial,50)
-pointsObject:setTextColor(27/255, 71/255, 144/255)
+pointsObject = display.newText("", 500, 250, Arial, 50)
+pointsObject:setTextColor(27/255, 144/255, 35/255)
 
 --create the correct text object and make it invisible
 correctObject = display.newText("Correct!", display.contentWidth/2, display.contentHeight*2/3, nil, 50)
@@ -134,9 +195,39 @@ numericField.inputType = "number"
 --add the event listener for the numeric field
 numericField:addEventListener("userInput", NumericFieldListener)
 
+--create the lives to display on the screen
+heart1 = display.newImageRect("Images/heart.png", 100, 100)
+heart1.x = display.contentWidth * 7 / 8
+heart1.y = display.contentHeight * 1 / 7
+
+heart2 = display.newImageRect("Images/heart.png", 100, 100)
+heart2.x = display.contentWidth * 6 / 8
+heart2.y = display.contentHeight * 1 / 7
+
+heart3 = display.newImageRect("Images/heart.png", 100, 100)
+heart3.x = display.contentWidth * 5 / 8
+heart3.y = display.contentHeight * 1 / 7
+
+heart4 = display.newImageRect("Images/heart.png", 100, 100)
+heart4.x = display.contentWidth * 4 / 8
+heart4.y = display.contentHeight * 1 / 7
+
+clockText = display.newText("", 100, 100, Arial,70)
+clockText:setTextColor(114/255, 32/255, 27/255)
+
+gameOver = display.newImageRect("Images/gameOver.png", 1100, 1100)
+gameOver.x = 500
+gameOver.y = 400
+gameOver.isVisible = false
+
+
 ----------------------------------------------------------------------------------------------
 --FUNCTION CALLS
 ----------------------------------------------------------------------------------------------
 
 --call the function to ask the question
 AskQuestion()
+
+UpdateTime()
+
+StartTimer()
